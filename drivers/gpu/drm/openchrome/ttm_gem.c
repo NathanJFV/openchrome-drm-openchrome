81764de585929cc4cc1476ba0500265b5480e125
Kevin Brace <kevinbrace@gmx.com>
2017-08-30 10:47:34 -0700

    Adjust via_bo_create input parameter list
    Renaming and changing the order to make it similar to ttm_bo_init.
    
    Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

--- drivers/gpu/drm/openchrome/ttm_gem.c
+++ drivers/gpu/drm/openchrome/ttm_gem.c
@@ -90,8 +90,9 @@ ttm_gem_create(struct drm_device *dev, struct ttm_bo_device *bdev,
 	size = round_up(size, byte_align);
 	size = ALIGN(size, page_align);
 
-	ret = via_bo_create(bdev, size, origin, types, byte_align,
-			      page_align, interruptible, NULL, NULL, &bo);
+	ret = via_bo_create(bdev, &bo, size, origin, types,
+				byte_align, page_align,
+				interruptible, NULL, NULL);
 	if (ret) {
 		DRM_ERROR("Failed to create buffer object\n");
 		return ERR_PTR(ret);
