55d3aa8f6d01ca5fec671459da0c17c9ddb0bea2
Kevin Brace <kevinbrace@gmx.com>
2017-10-22 20:24:04 -0500

    Version bumped to 3.0.50
    This version adds initial support for DVI provided by CX700 / VX700
    and VX800 chipsets’ integrated TMDS transmitter. Standby resume was
    tested with the DVI, and it is working.
    
    Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

--- drivers/gpu/drm/openchrome/via_drv.h
+++ drivers/gpu/drm/openchrome/via_drv.h
@@ -30,11 +30,11 @@
 #define DRIVER_AUTHOR       "OpenChrome Project"
 #define DRIVER_NAME         "openchrome"
 #define DRIVER_DESC         "OpenChrome DRM for VIA Technologies Chrome IGP"
-#define DRIVER_DATE         "20171015"
+#define DRIVER_DATE         "20171022"
 
 #define DRIVER_MAJOR		3
 #define DRIVER_MINOR		0
-#define DRIVER_PATCHLEVEL	49
+#define DRIVER_PATCHLEVEL	50
 
 #include <linux/module.h>
 
