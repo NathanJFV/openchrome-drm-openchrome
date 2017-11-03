b86403791b2578ffc524cbe96d263dbc0b076d30
Kevin Brace <kevinbrace@gmx.com>
2017-11-02 23:48:16 -0700

    drm/openchrome: Version bumped to 3.0.54
    This version fixes the I2C display detection regression introduced
    by Version 3.0.52.
    
    Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

--- drivers/gpu/drm/openchrome/via_drv.h
+++ drivers/gpu/drm/openchrome/via_drv.h
@@ -30,11 +30,11 @@
 #define DRIVER_AUTHOR       "OpenChrome Project"
 #define DRIVER_NAME         "openchrome"
 #define DRIVER_DESC         "OpenChrome DRM for VIA Technologies Chrome IGP"
-#define DRIVER_DATE         "20171030"
+#define DRIVER_DATE         "20171102"
 
 #define DRIVER_MAJOR		3
 #define DRIVER_MINOR		0
-#define DRIVER_PATCHLEVEL	53
+#define DRIVER_PATCHLEVEL	54
 
 #include <linux/module.h>
 
