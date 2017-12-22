07cc6a9774ad396d62b4d915a65d8dcc183e20c1
Kevin Brace <kevinbrace@gmx.com>
2017-12-22 01:29:54 -0600

    drm/openchrome: Version bumped to 3.0.69
    Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

--- drivers/gpu/drm/openchrome/via_drv.h
+++ drivers/gpu/drm/openchrome/via_drv.h
@@ -30,11 +30,11 @@
 #define DRIVER_AUTHOR       "OpenChrome Project"
 #define DRIVER_NAME         "openchrome"
 #define DRIVER_DESC         "OpenChrome DRM for VIA Technologies Chrome IGP"
-#define DRIVER_DATE         "20171221"
+#define DRIVER_DATE         "20171222"
 
 #define DRIVER_MAJOR		3
 #define DRIVER_MINOR		0
-#define DRIVER_PATCHLEVEL	68
+#define DRIVER_PATCHLEVEL	69
 
 #include <linux/module.h>
 
